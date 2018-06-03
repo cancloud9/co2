7880c33d4b67febc3706df570b037340b8d8ca3e
萬俊瑋 <wei840222@gmail.com>
2018-06-03 15:38:35 +0800

    add k-nn classify

--- readArduinoData.py
+++ readArduinoData.py
@@ -4,8 +4,8 @@ ser=serial.Serial('COM3',9600)
 
 with open('sensorData.csv','w') as outputFile:
     outputFile.write('light, CO2\n')
-    for i in range(10):
-        sensorData=str(ser.readline())[2:-5]+', 0'
+    for i in range(100):
+        sensorData=str(ser.readline())[2:-5]
         outputFile.write(sensorData+'\n')
 
 ser.close()
